47b22b10bed7178bc4abb99ccf6741a3ef206df2
shimeji221418 <shimeji2214@gmail.com>
2022-11-18 21:36:42 +0900

    create Usermodel and Postmodel

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users, only: [:index, :show, :update, :destroy]
+      resources :posts
+    end
+  end
 end
